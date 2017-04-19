49e8f99a5b6ac9a67bc87339c63af0e628a5b545
tigerb <624661874@qq.com>
2017-04-19 22:18:59 +0800

    feat(nosql/Memcached): add Memcached support :sparkles:

---
+++ framework/nosql/Memcached.php
@@ -0,0 +1,35 @@
+<?php
+/********************************************
+ *                Easy PHP                  *
+ *                                          *
+ * A lightweight PHP framework for studying *
+ *                                          *
+ *                 TIERGB                   *
+ *      <https://github.com/TIGERB>         *
+ *                                          *
+ ********************************************/
+
+namespace Framework\Nosql;
+
+use Framework\App;
+use Memcached as rootMemcached;
+
+/**
+ * memcached操作类
+ *
+ * @author TIERGB <https://github.com/TIGERB>
+ */
+class Memcached
+{
+    /**
+     * 构造函数
+     */
+    public function __construct()
+    {
+        $config = App::$container->getSingle('config');
+        $config = $config->config['nosql']['Memcached'];
+        $redis = new rootMemcached();
+        $redis->addServer($config['host'], $config['port']);
+        App::$container->setSingle('memcached', $redis);
+    }
+}
