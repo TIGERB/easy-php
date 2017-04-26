723675aa3147ff6b90b2dd59f20182e60e70a5d1
tigerb <624661874@qq.com>
2017-04-26 13:50:32 +0800

    feat(docs): add swagger :bulb:

---
+++ docs/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# git clone swagger
+# if [ ! command git ]; then 
+# echo ""
+# echo -e "\033[41mPlease install git \033[0m"
+# exit 1
+# fi
+
+git clone https://github.com/swagger-api/swagger-ui.git
