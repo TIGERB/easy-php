ec750cad3796bb75e4d61c47a52d1121fdc946c5
tigerb <624661874@qq.com>
2017-05-05 17:41:08 +0800

    docs(App): optimize note for code :ok_hand:

--- framework/run.php
+++ framework/run.php
@@ -20,71 +20,108 @@ use Framework\Exceptions\CoreHttpException;
 use Framework\Request;
 use Framework\Response;
 
-// 引入框架文件
+/**
+ * 引入框架文件
+ *
+ * Require framework
+ */
 require(__DIR__ . '/App.php');
 
 try {
+    //------------------------------------------------------------------------//
+    //                                  INIT                                  //
+    //------------------------------------------------------------------------//
+
     /**
      * 初始化应用
+     *
+     * Init framework
      */
     $app = new Framework\App(__DIR__ . '/..', function () {
         return require(__DIR__ . '/Load.php');
     });
 
+    //-----------------------------------------------------------------------//
+    //                         LOADING HANDLE MODULE                         //
+    //-----------------------------------------------------------------------//
+
     /**
      * 挂载handles
+     *
+     * Load all kinds of handles
      */
     $app->load(function () {
-        // 加载日志处理机制
+        // 加载日志处理机制 Loading log handle
         return new LogHandle();
     });
-    
+
     $app->load(function () {
-        // 加载错误处理机制
+        // 加载错误处理机制 Loading error handle
         return new ErrorHandle();
     });
 
     // $app->load(function () {
     //     //  加载异常处理机制　由于本文件全局catch了异常　所以不存在未捕获异常
     //     //　可省略注册未捕获异常Handle
+    //     //  Loading exception handle.
+    //     //  I'm not used it, because this file catch all exception
     //     return new ExceptionHandle();
     // });
 
     $app->load(function () {
-        // 加载预定义配置机制
+        // 加载预定义配置机制 Loading config handle
         return new ConfigHandle();
     });
 
     $app->load(function () {
-        // 加载nosql机制
+        // 加载nosql机制 Loading nosql handle
         return new NosqlHandle();
     });
 
     $app->load(function () {
-        // 加载用户自定义机制
+        // 加载用户自定义机制 Loading user-defined handle
         return new UserDefinedHandle();
     });
 
     $app->load(function () {
-        // 加载路由机制
+        // 加载路由机制 Loading route handle
         return new RouterHandle();
     });
 
+    //-----------------------------------------------------------------------//
+    //                              START APP                                //
+    //-----------------------------------------------------------------------//
+
     /**
      * 启动应用
+     *
+     * Start framework
      */
     $app->run(function () use ($app) {
         return new Request($app);
     });
 
+    //-----------------------------------------------------------------------//
+    //                          STOP APP & RESPONSE                          //
+    //-----------------------------------------------------------------------//
+
     /**
      * 响应结果
-     * 应用生命周期结束　
+     *
+     * Reponse
+     *
+     * 应用生命周期结束
+     *
+     * End
      */
     $app->response(function () {
         return new Response();
     });
 } catch (CoreHttpException $e) {
-    // 捕获异常
+    /**
+     * 捕获异常
+     *
+     * Catch exception
+     */
     $e->reponse();
 }
